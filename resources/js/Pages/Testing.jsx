86c60d887bf5c91a4bfe905277df5a7611b50783
Rino Arif Budiyanto <rino.asuszen2@gmail.com>
2025-02-14 20:51:44 +0700

    app layout

---
+++ resources/js/Pages/Testing.jsx
@@ -0,0 +1,19 @@
+import AppLayout from '@/Layouts/AppLayout';
+
+export default function Testing() {
+    return (
+        <AppLayout>
+            <h1>Hai cuy</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, eveniet iste. Nam non fugit tenetur
+                aspernatur id cum sit repudiandae ea dicta delectus. Assumenda non tenetur cupiditate libero, minima ut
+                iusto qui possimus corporis totam magni ad adipisci ratione officia odio porro quisquam rem consequatur
+                eligendi repellat molestias! Velit consequatur et autem quo neque reprehenderit iusto doloremque
+                incidunt, corrupti sit facilis corporis eligendi eum consectetur soluta deserunt accusantium minus quas
+                culpa, fugiat provident tenetur laborum necessitatibus libero? Suscipit voluptatem illum quo sunt qui
+                eveniet enim eaque maxime assumenda voluptas repellat reprehenderit corrupti, amet sint minima nulla,
+                repudiandae, omnis deserunt deleniti.
+            </p>
+        </AppLayout>
+    );
+}
--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,27 +1,26 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export const STATUS = {
-  
-  TODO: 'To Do',
-  INPROGRESS: 'In Progress',
-  ONREVIEW: 'On Review',
-  DONE: 'Done',
-  UNKNOWN: 'Unkwown'
+    TODO: 'To Do',
+    INPROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOWN: 'Unkwown',
 };
 
 export const PRIORITY = {
-  URGENT: 'Urgent',
-  HIGH: "High",
-  MEDIUM: "Medium",
-  LOW: "Low",
-  UNKNOWN: "Unknown",
+    URGENT: 'Urgent',
+    HIGH: 'High',
+    MEDIUM: 'Medium',
+    LOW: 'Low',
+    UNKNOWN: 'Unknown',
 };
 
-export function flashMessage(params){
-  return params.props.flash_message; 
+export function flashMessage(params) {
+    return params.props.flash_message;
 }
