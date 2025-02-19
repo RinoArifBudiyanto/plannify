58b52e49122c718324e159748f4e251e636abe3b
Rino Arif Budiyanto <rino.asuszen2@gmail.com>
2025-02-19 09:28:38 +0700

    sidebar

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -82,7 +82,7 @@ export default function AppLayout({ children, title }) {
                                         <li>
                                             <Link
                                                 href="#"
-                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-500"
+                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-400"
                                             >
                                                 <PiHouse className="h-6 w-6 shrink-0 text-foreground" />
                                                 Dashboard
@@ -91,7 +91,7 @@ export default function AppLayout({ children, title }) {
                                         <li>
                                             <Link
                                                 href="#"
-                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-500"
+                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-400"
                                             >
                                                 <PiUser className="h-6 w-6 shrink-0 text-foreground" />
                                                 People
@@ -100,7 +100,7 @@ export default function AppLayout({ children, title }) {
                                         <li>
                                             <Link
                                                 href="#"
-                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-500"
+                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-400"
                                             >
                                                 <PiSquaresFour className="h-6 w-6 shrink-0 text-foreground" />
                                                 My Tasks
@@ -109,7 +109,7 @@ export default function AppLayout({ children, title }) {
                                         <li>
                                             <Link
                                                 href="#"
-                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-500"
+                                                className="group flex gap-3 gap-x-3 rounded-md p-3 font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-red-400"
                                             >
                                                 <PiLockKeyOpen className="h-6 w-6 shrink-0 text-foreground" />
                                                 Log Out
@@ -117,18 +117,48 @@ export default function AppLayout({ children, title }) {
                                         </li>
                                     </ul>
                                 </li>
-                                {/* workspaces */}
+                                {/* workspaces start */}
                                 <li>
-                                    <div className='flex items-center justify-between'>
-                                        <div>
+                                    <div className="flex items-center justify-between">
+                                        <div className="text-xs font-semibold uppercase leading-relaxed text-foreground">
                                             Workspaces
                                         </div>
                                         <Link>
-                                        
+                                            <PiPlus className="h-4 w-4 text-foreground hover:text-red-400" />
                                         </Link>
                                     </div>
+                                    <ul role="list" className="-mx-2 mt-2 space-y-2">
+                                        <li>
+                                            <Link
+                                                href="#"
+                                                className="group flex w-full gap-x-3 rounded-md p-3 font-semibold leading-relaxed text-foreground hover:bg-red-400"
+                                            >
+                                                <span className='border-foreground text-foreground flex h-6 w-6  shrink-0 items-center justify-center rounded-lg border bg-white'>B</span>
+                                                <span className='truncate'>Back End Developer</span>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                href="#"
+                                                className="group flex w-full gap-x-3 rounded-md p-3 font-semibold leading-relaxed text-foreground hover:bg-red-400"
+                                            >
+                                                <span className='border-foreground text-foreground flex h-6 w-6  shrink-0 items-center justify-center rounded-lg border bg-white'>F</span>
+                                                <span className='truncate'>Front End Developer</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
                                 </li>
-                                <li className="-mx-6 mt-auto">{/* profile */}</li>
+                                {/* Workspace end */}
+                                {/* profile start*/}
+                                <li className="-mx-6 mt-auto">
+                                    <Link href=''
+                                    className='flex items-center px-6 py-3 text-sm font-semibold leading-relaxed gap-x-4 text-foreground hover:bg-gray-200'>
+                                    <Avatar>
+                                        <AvatarFallback></AvatarFallback>
+                                    </Avatar>
+                                    <span>Rino Arif Budiyanto</span>
+                                    </Link>
+                                    </li>
                             </ul>
                         </nav>
                     </div>
